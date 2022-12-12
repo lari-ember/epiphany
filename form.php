9b926b2d95cc5237254920cca4d95df98a1efafd
Larissa Ember <74555040+lari-ember@users.noreply.github.com>
2022-12-12 14:11:27 -0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,11 @@
+<?php
+    $name=$_POST['name'];
+    $age=$_POST['age'];
+    $city=$_POST['city'];
+  
+    echo "NAME-SUBMITTED : $name <br>";
+  
+    echo "AGE-SUBMITTED :  $age <br>";
+  
+    echo "CITY-SUBMITTED:  $city";
+?>
